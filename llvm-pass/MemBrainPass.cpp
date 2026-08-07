af45f7fd07975cf19eed5cfcd448cbec756f5e80
Maksym Shcherba <maksym.shcherba@lnu.edu.ua>
2026-08-07 04:05:33 -0600

    feat(llvm-pass): add site_id tagging and allocation instruction rewriting
    - Rewrote malloc, calloc, realloc, and operator new calls to membrain_alloc(size, site_id)
    - Implemented getAllocationSize() to handle calloc (num * size), realloc/aligned_alloc (arg 1), and posix_memalign (arg 2)
    - Used stripPointerCasts() on called operands to resolve bitcasted allocation functions
    - Added automatic allocation_sites.json metadata generation with OF_Append support for multi-file builds
    - Registered single PipelineStartEP callback to avoid duplicate pass executions

--- llvm-pass/MemBrainPass.cpp
+++ llvm-pass/MemBrainPass.cpp
@@ -1,48 +1,121 @@
-// MemBrain LLVM Pass Plugin - Allocation Scanner Skeleton
-#include "llvm/IR/Function.h"
+// MemBrain LLVM Pass Plugin - Site ID Tagging & IR Rewriting
+#include "llvm/IR/DebugInfoMetadata.h"
+#include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/FormatVariadic.h"
+#include "llvm/Support/JSON.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
 
 namespace {
 
-static bool isAllocationFunction(StringRef name) {
+static bool isAllocationCall(CallBase *CB) {
+    Value *calledOp = CB->getCalledOperand()->stripPointerCasts();
+    Function *F = dyn_cast<Function>(calledOp);
+    if (!F) return false;
+
+    StringRef name = F->getName();
     return name == "malloc" || name == "calloc" || name == "realloc" ||
            name == "posix_memalign" || name == "aligned_alloc" ||
-           name == "_Znwm" || name == "_Znam" || name == "f90_alloc";
+           name.starts_with("_Znw") || name.starts_with("_Zna");
+}
+
+static Value *getAllocationSize(CallBase *CB, IRBuilder<> &Builder, Type *sizeTy) {
+    Value *calledOp = CB->getCalledOperand()->stripPointerCasts();
+    Function *F = dyn_cast<Function>(calledOp);
+    if (!F) return CB->getArgOperand(0);
+
+    StringRef name = F->getName();
+    if (name == "calloc") {
+        Value *num = Builder.CreateZExtOrTrunc(CB->getArgOperand(0), sizeTy);
+        Value *elemSize = Builder.CreateZExtOrTrunc(CB->getArgOperand(1), sizeTy);
+        return Builder.CreateMul(num, elemSize);
+    }
+    if (name == "realloc" || name == "aligned_alloc") {
+        return CB->getArgOperand(1);
+    }
+    if (name == "posix_memalign") {
+        return CB->getArgOperand(2);
+    }
+    return CB->getArgOperand(0);
 }
 
 struct MemBrainPass : public PassInfoMixin<MemBrainPass> {
     PreservedAnalyses run(Module &M, ModuleAnalysisManager &MAM) {
-        uint32_t foundCount = 0;
+        LLVMContext &Ctx = M.getContext();
+        uint32_t siteId = 1;
+        bool modified = false;
+
+        // Function prototype for membrain_alloc(size_t size, uint32_t site_id)
+        Type *sizeTy = Type::getInt64Ty(Ctx);
+        Type *int32Ty = Type::getInt32Ty(Ctx);
+        Type *ptrTy = PointerType::getUnqual(Ctx);
+        FunctionType *hookTy = FunctionType::get(ptrTy, {sizeTy, int32Ty}, false);
+        FunctionCallee membrainAllocCallee = M.getOrInsertFunction("membrain_alloc", hookTy);
+
+        json::Array siteArray;
 
         for (Function &F : M) {
-            if (F.isDeclaration()) continue;
+            if (F.isDeclaration() || F.getName().starts_with("membrain_")) continue;
 
             for (BasicBlock &BB : F) {
-                for (Instruction &I : BB) {
+                for (auto InstIt = BB.begin(); InstIt != BB.end(); ) {
+                    Instruction &I = *InstIt++;
                     auto *CB = dyn_cast<CallBase>(&I);
                     if (!CB) continue;
 
-                    Function *calledFunc = CB->getCalledFunction();
-                    if (!calledFunc) continue;
+                    if (isAllocationCall(CB)) {
+                        IRBuilder<> Builder(CB);
+                        Value *rawSizeVal = getAllocationSize(CB, Builder, sizeTy);
+                        Value *sizeVal = rawSizeVal;
+                        if (sizeVal->getType() != sizeTy) {
+                            sizeVal = Builder.CreateZExtOrTrunc(sizeVal, sizeTy);
+                        }
 
-                    StringRef funcName = calledFunc->getName();
-                    if (isAllocationFunction(funcName)) {
-                        foundCount++;
-                        errs() << "[MemBrainPass] Identified allocation call to '"
-                               << funcName << "' in function '" << F.getName() << "'\n";
+                        Value *siteIdVal = ConstantInt::get(int32Ty, siteId);
+                        CallInst *newCall = Builder.CreateCall(membrainAllocCallee, {sizeVal, siteIdVal});
+                        newCall->setDebugLoc(CB->getDebugLoc());
+
+                        // Replace uses of original allocation with newCall
+                        CB->replaceAllUsesWith(newCall);
+                        CB->eraseFromParent();
+                        modified = true;
+
+                        // Metadata recording
+                        json::Object siteObj;
+                        siteObj["site_id"] = siteId;
+                        siteObj["function"] = F.getName().str();
+
+                        if (DILocation *Loc = newCall->getDebugLoc()) {
+                            siteObj["file"] = Loc->getFilename().str();
+                            siteObj["line"] = Loc->getLine();
+                        } else {
+                            siteObj["file"] = "unknown";
+                            siteObj["line"] = 0;
+                        }
+                        siteArray.push_back(std::move(siteObj));
+
+                        siteId++;
                     }
                 }
             }
         }
 
-        errs() << "[MemBrainPass] Total allocation sites detected: " << foundCount << "\n";
-        return PreservedAnalyses::all();
+        if (modified) {
+            std::error_code EC;
+            raw_fd_ostream os("allocation_sites.json", EC, sys::fs::OF_Append);
+            if (!EC) {
+                os << formatv("{0:2}", json::Value(std::move(siteArray)));
+            }
+        }
+
+
+        return modified ? PreservedAnalyses::none() : PreservedAnalyses::all();
     }
 };
 
@@ -53,10 +126,6 @@ extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo llvmGetPassPluginIn
         LLVM_PLUGIN_API_VERSION, "MemBrainPass", "v1.0",
         [](PassBuilder &PB) {
             PB.registerPipelineStartEPCallback(
-                [](ModulePassManager &MPM, OptimizationLevel Level) {
-                    MPM.addPass(MemBrainPass());
-                });
-            PB.registerOptimizerLastEPCallback(
                 [](ModulePassManager &MPM, OptimizationLevel Level) {
                     MPM.addPass(MemBrainPass());
                 });
@@ -72,4 +141,3 @@ extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo llvmGetPassPluginIn
         }
     };
 }
-
